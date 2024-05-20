a0b9bb68a39f8cf04bc31c4aad01d79cde6233b4
Tebogo Mofokeng <TMofokeng@moseskotane.gov.za>
2024-05-20 09:48:59 +0200

    Initial commit

---
+++ src/main/java/com/example/ShopNWatchAPI/ShopNWatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopNWatchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNWatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopNWatchApiApplication.class, args);
+	}
+
+}
